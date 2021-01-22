11f1486452cf3d8a575f43ca3b590df3379a102a
webpagearshi <arshisaleh@gmail.com>
2021-01-23 00:00:13 +0530

    feat: added train.py script

---
+++ train.py
@@ -0,0 +1,16 @@
+
+import argparse
+import os
+import numpy as np
+#from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+
+
+
+
